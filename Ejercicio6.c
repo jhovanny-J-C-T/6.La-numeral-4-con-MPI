b160ab6d14900d2f8b0cbb0fbcc97a3b0724e561
jhovanny-J-C-T <56840398+jhovanny-J-C-T@users.noreply.github.com>
2023-12-11 12:44:32 -0400

    Ejercicio6.c

--- Ejercicio6.c
+++ Ejercicio6.c
@@ -2,30 +2,43 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-int main(int argc, char *argv[]) {
-    int rank, size;
-    const int cantidadNumeros = 100000;
-    int *numeros = (int *)malloc(cantidadNumeros * sizeof(int));
-    int *resultados = (int *)malloc(cantidadNumeros * sizeof(int));
+void CalcularNumero(int index, int numero, int *resultados);
 
+int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
+
+    int rank, size;
+
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Dividir el trabajo entre los procesos
-    int inicio = rank * (cantidadNumeros / size);
-    int fin = (rank + 1) * (cantidadNumeros / size);
-    for (int i = inicio; i < fin; i++) {
-        numeros[i] = i + 1;
-        resultados[i] = (numeros[i] % 2 == 0) ? numeros[i] * 2 : numeros[i] * 2 + 1;
+    // Definir la cantidad de números en la serie
+    const int cantidadNumeros = 100000;
+
+    // Calcular la cantidad de números por proceso
+    const int numerosPorProceso = cantidadNumeros / size;
+
+    // Inicializar arreglos para almacenar los números y los resultados
+    int *numeros = (int *)malloc(numerosPorProceso * sizeof(int));
+    int *resultados = (int *)malloc(numerosPorProceso * sizeof(int));
+
+    // Crear y ejecutar tareas en paralelo utilizando MPI_Scatter
+    MPI_Scatter(NULL, 0, MPI_INT, numeros, numerosPorProceso, MPI_INT, 0, MPI_COMM_WORLD);
+
+    for (int i = 0; i < numerosPorProceso; i++) {
+        // Asignar valores a los números
+        numeros[i] = rank * numerosPorProceso + i + 1;
+
+        // Calcular y almacenar el resultado para cada número
+        CalcularNumero(i, numeros[i], resultados);
     }
 
-    // Recopilar los resultados en el proceso 0
-    MPI_Gather(resultados + inicio, cantidadNumeros / size, MPI_INT, resultados, cantidadNumeros / size, MPI_INT, 0, MPI_COMM_WORLD);
+    // Recopilar resultados en el proceso raíz
+    MPI_Gather(resultados, numerosPorProceso, MPI_INT, NULL, 0, MPI_INT, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-        // Mostrar la serie generada
-        printf("Serie generada con 100000 posiciones: ");
+        // Mostrar toda la serie en el proceso raíz
+        printf("Serie generada con 100000 posiciones:\n");
         for (int i = 0; i < cantidadNumeros; i++) {
             printf("%d", resultados[i]);
             if (i < cantidadNumeros - 1) {
@@ -35,9 +48,15 @@ int main(int argc, char *argv[]) {
         printf("\n");
     }
 
+    // Liberar memoria
     free(numeros);
     free(resultados);
 
     MPI_Finalize();
     return 0;
+}
+
+// Método para calcular y asignar el resultado según si el número es par o impar
+void CalcularNumero(int index, int numero, int *resultados) {
+    resultados[index] = (numero % 2 == 0) ? numero * 2 : numero * 2 + 1;
 }
